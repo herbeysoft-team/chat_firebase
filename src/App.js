0475955d25e49a066afdff421cc71bd32b1f9cb8
herbeysoft-team <herbeysoft@gmail.com>
2022-11-29 22:03:38 +0100

    Issue with Storage solved

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 import './style.scss';
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children
+  };
+
   return (
-    <div>
-      <Register/>
-      {/* <Login/> */}
-      {/* <Home /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
